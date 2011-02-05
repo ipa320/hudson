5957563ed4c2d454c33c7b25b4c7491ad4f30c43
ipa320 <florian.weisshardt@ipa.fhg.de>
2011-02-05 16:57:31 +0100

    deleted unused file

--- job_config/default_jobs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-./create_job.sh diamondback ipa-fmw cob_extern
-./create_job.sh diamondback ipa-fmw cob_common
-./create_job.sh diamondback ipa-fmw cob_driver
-./create_job.sh diamondback ipa-fmw cob_simulation
-./create_job.sh diamondback ipa-fmw cob_apps
-./create_gazebo_job.sh diamondback ipa-fmw
