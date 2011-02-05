653065ea0806256c1622d50e709774f200152d0f
ipa320 <florian.weisshardt@ipa.fhg.de>
2011-02-05 20:09:32 +0100

    using setup.bash

--- run/hudson.sh
+++ run/hudson.sh
@@ -25,11 +25,10 @@ sed -i "s/---REPOSITORY---/$REPOSITORY/g" $WORKSPACE/../$REPOSITORY.rosinstall
 
 # perform clean rosinstall
 rm $WORKSPACE/.rosinstall
-rm $WORKSPACE/../setup.sh
 rosinstall $WORKSPACE $WORKSPACE/../$REPOSITORY.rosinstall $WORKSPACE --delete-changed-uris
 
 # setup ROS environment
-. $WORKSPACE/setup.sh
+. $WORKSPACE/setup.bash
 
 # define amount of ros prozesses during build for multi-prozessor machines
 COUNT=$(cat /proc/cpuinfo | grep 'processor' | wc -l)
