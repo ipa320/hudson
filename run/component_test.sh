30a2e2f01672475ef2717b9d3b508b75573f028a
fmw-jk <jannik.kett@googlemail.com>
2011-06-10 16:50:36 +0200

    debugging

--- run/component_test.sh
+++ run/component_test.sh
@@ -16,36 +16,34 @@ pid_script_server_raw="$( jobs -l | grep ]+ )"
 pid_script_server_raw=${pid_script_server_raw%% Running*}
 pid_script_server=${pid_script_server_raw##*]+ }
 
+do_test(){
+    #set test parameter for current component and target
+    rosparam set /component_test/component "$1"
+    rosparam set /component_test/command_topic /"$1"_controller/command
+    rosparam set /component_test/state_topic /"$1"_controller/state
+    rosparam set /component_test/target "$2"
+    sleep 1s
+    echo "Component: " $1 " / Target: " $2
+    #write introduction for current test to test file
+    echo "-------------------------------------------" >> $WORKSPACE/../component_test_result.txt
+    echo "Component: " $1 " / Target: " $2 >> $WORKSPACE/../component_test_result.txt
+    echo "-------------------------------------------" >> $WORKSPACE/../component_test_result.txt
+    #start test and write results to text file
+    test/trajectory_test.py >> $WORKSPACE/../component_test_result.txt
+    sleep 1s
+    cat $WORKSPACE/../component_test_result.txt                  # to be deleted
+}
+
 #change to cob_component_test package
 cd "$( rospack find cob_component_test )"/ros
 
 #create/overwrite text file for test results
 echo "TEST RESULTS" > $WORKSPACE/../component_test_result.txt
-cat $WORKSPACE/../component_test_result.txt
+cat $WORKSPACE/../component_test_result.txt                      # to be deleted
 
 #set static test parameters
 rosparam set /component_test/wait_time 10.0
 rosparam set /component_test/error_range 1.0
-
-do_test(){
-    #set test parameter for current component and target
-    rosparam set /component_test/component $1
-    rosparam set /component_test/command_topic /$1_controller/command
-    rosparam set /component_test/state_topic /$1_controller/state
-    rosparam set /component_test/target $2
-    sleep 1s
-    echo "Component: " $1 " / Target: " $2
-    #write introduction for current test to test file
-    cat >>$WORKSPACE/../component_test_result.txt << "EOF" 
-    -------------------------------------------
-    Component: $1 / Target: $2
-    -------------------------------------------
-    EOF
-    #start test and write results to text file
-    test/trajectory_test.py >> $WORKSPACE/../component_test_result.txt
-    sleep 1s
-    cat $WORKSPACE/../component_test_result.txt
-}
 
 #tests for each component
 do_test arm home
