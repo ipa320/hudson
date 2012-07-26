ea5831a3d65ba3c66c1be5610e9e542c77a56c24
fmw-jk <jannik.kett@googlemail.com>
2012-07-26 11:35:27 +0200

    cleaned up code

--- wg_jenkins_stack/job_generation/src/job_generation/run_auto_stack_prerelease_devel.py
+++ wg_jenkins_stack/job_generation/src/job_generation/run_auto_stack_prerelease_devel.py
@@ -34,13 +34,16 @@ def main():
         env = get_environment()
         env['ROS_PACKAGE_PATH'] = '%s:%s:/opt/ros/%s/stacks'%(env['INSTALL_DIR']+'/'+STACK_DIR,
                                                                  env['INSTALL_DIR']+'/'+DEPENDS_DIR,
-                                                                 options.rosdistro) # env['INSTALL_DIR']+'/'+DEPENDS_ON_DIR,
+                                                                 options.rosdistro) 
+                                                                 # env['INSTALL_DIR']+'/'+DEPENDS_ON_DIR,
 
         if 'ros' in options.stack:
             env['ROS_ROOT'] = env['INSTALL_DIR']+'/'+STACK_DIR+'/ros'
             print "We're building ROS, so setting the ROS_ROOT to %s"%(env['ROS_ROOT'])
+        
         else:
             env['ROS_ROOT'] = '/opt/ros/%s/ros'%options.rosdistro
+        
         env['PYTHONPATH'] = env['ROS_ROOT']+'/core/roslib/src'
         env['PATH'] = '/opt/ros/%s/ros/bin:%s'%(options.rosdistro, os.environ['PATH'])
         env['ROS_PARALLEL_JOBS'] = '-j4'
@@ -51,61 +54,42 @@ def main():
         rosdistro_obj = rosdistro.Distro(get_rosdistro_file(options.rosdistro))
         print 'Operating on ROS distro %s'%rosdistro_obj.release_name
 
+        # update sources
         call('sudo apt-get update', env)
 
         # Install the stacks to test from source
         print '\n==================================================================================='
         print 'Installing %s to test from source\n'%str(options.stack)
         sys.stdout.flush()
-        ###rosinstall = ''
         for stack in options.stack:
             if not stack_forked(options.githubuser, stack):
-                raise Exception('  Stack %s is not forked for user %s! Fork stack on github.com to run job'%(stack, options.githubuser))
+                raise Exception('  Stack %s is not forked for user %s! Fork stack on github.com to run job'
+                                    %(stack, options.githubuser))
              
             #Clone stack from github
-            ###rosinstall += stack_origin(rosdistro_obj, rosinstall, stack, options.githubuser, STACK_DIR, env)
             if get_stack_membership(stack) == "private":
-                call('git clone git@github.com:%s/%s.git %s/%s'%(options.githubuser, stack, STACK_DIR, stack), env, 'Clone stack [%s] for testing'%(stack))
+                call('git clone git@github.com:%s/%s.git %s/%s'
+                        %(options.githubuser, stack, STACK_DIR, stack), 
+                        env, 'Clone stack [%s] for testing'%(stack))
             else:
-                call('git clone git://github.com/%s/%s.git %s/%s'%(options.githubuser, stack, STACK_DIR, stack), env, 'Clone stack [%s] for testing'%(stack))
-            
-        ###if rosinstall != '': # call rosinstall command
-        ###    print '\n==================================================================================='
-        ###    rosinstall_file = '%s.rosinstall'%STACK_DIR
-        ###    print 'Generating rosinstall file [%s]'%(rosinstall_file)
-        ###    print 'Contents:\n\n'+rosinstall+'\n\n'
-        ###    with open(rosinstall_file, 'w') as f:
-        ###        f.write(rosinstall)
-        ###    print 'rosinstall file [%s] generated'%(rosinstall_file)
-        ###    print STACK_DIR
-        ###    print options.rosdistro
-        ###    call('rosinstall --rosdep-yes %s /opt/ros/%s %s'%(STACK_DIR, options.rosdistro, rosinstall_file), env,
-        ###         'Install the stacks to test from source.')
+                call('git clone git://github.com/%s/%s.git %s/%s'
+                        %(options.githubuser, stack, STACK_DIR, stack), 
+                        env, 'Clone stack [%s] for testing'%(stack))
             
         
         # get all stack dependencies of stacks we're testing
-        ###rosinstall = ''
         depends_all = {"public" : [], "private" : [], "other" : []}
         for stack in options.stack:
             print '\n==================================================================================='
             print 'Calculating and getting all stack dependencies of %s\n'%stack
+            # get direct dependencies of stack
             depends_one = get_depends_one(stack, STACK_DIR)
 
-            #get all depends recursivly and clone or install it
+            # get all dependencies recursivly and clone or install it
             print get_depends_all(depends_one, depends_all, options.githubuser, DEPENDS_DIR, rosdistro_obj, env) 
             sys.stdout.flush()
 
 
-            ###for dep in depends_one:
-                #clone if ipa stack or install if external stack
-                #get_stack(rosdistro_obj, dep, options.githubuser, DEPENDS_DIR, env)
-
-
-                ###depends_all_list = []
-                ###[[depends_all_list.append(value) for value in valuelist] for valuelist in depends_all.itervalues()]
-                ###if not d in options.stack and not d in depends_all_list:
-                ###    get_depends_all(d, depends_all, options.githubuser, 1)
-        
         print '\n==================================================================================='
         print 'Dependencies of %s:'%str(options.stack)
         print "  Private IPA stacks:"
@@ -118,33 +102,6 @@ def main():
         sys.stdout.flush()
 
 
-        ###if len(depends_all["private"]) > 0:#TODO released private stack???
-        ###    print '\n==================================================================================='
-        ###    print 'Cloning private github fork(s)\n'
-        ###    for stack in depends_all["private"]:
-        ###        rosinstall += stack_origin(rosdistro_obj, rosinstall, stack, options.githubuser, DEPENDS_DIR, env)
-        ###            
-
-        ###if len(depends_all["public"]) > 0:
-        ###    print '\n==================================================================================='
-        ###    print 'Installing stack dependencies from public github fork\n'
-        ###    for stack in depends_all["public"]:
-        ###        rosinstall += stack_origin(rosdistro_obj, rosinstall, stack, options.githubuser, DEPENDS_DIR, env)
-        ###    
-        ###if rosinstall != '':
-        ###    rosinstall_file = '%s.rosinstall'%DEPENDS_DIR
-        ###    with open(rosinstall_file, 'w') as f:
-        ###        f.write(rosinstall)
-        ###    call('rosinstall --rosdep-yes %s /opt/ros/%s %s'%(DEPENDS_DIR, options.rosdistro, rosinstall_file), env,
-        ###         'Install the stack dependencies from source.')
-     
-        ###if len(depends_all["other"]) > 0:
-        ###    # Install Debian packages of stack dependencies
-        ###    print '\n==================================================================================='
-        ###    print 'Installing debian packages of "%s" dependencies:\n\n%s'%(str(options.stack), str(depends_all["other"]))
-        ###    call('sudo apt-get update', env)
-        ###    call('sudo apt-get install %s --yes'%(stacks_to_debs(depends_all["other"], options.rosdistro)), env)
- 
         depends_no = 0
         for i in iter(depends_all): depends_no += len(depends_all[i])
         if depends_no == 0:
@@ -169,7 +126,8 @@ def main():
         for r in range(0, int(options.repeat)+1):
             env['ROS_TEST_RESULTS_DIR'] = env['ROS_TEST_RESULTS_DIR'] + '/' + STACK_DIR + '_run_' + str(r)
             print '\nHUDSON HELPER CALL:'
-            helper = subprocess.Popen(('./ros_release/hudson/src/hudson_helper_fhg.py --dir-test %s --email %s build'%(STACK_DIR, options.email)).split(' '), env=env)
+            helper = subprocess.Popen(('./ros_release/hudson/src/hudson_helper_fhg.py --dir-test %s --email %s build'
+                                        %(STACK_DIR, options.email)).split(' '), env=env)
             helper.communicate()
             if helper.returncode != 0:
                 res = helper.returncode
@@ -177,7 +135,8 @@ def main():
             return res
         
         build_failures = os.path.join(os.environ['WORKSPACE'], 'build_output', 'buildfailures.txt')
-        build_failures_context = os.path.join(os.environ['WORKSPACE'], 'build_output', 'buildfailures-with-context.txt')
+        build_failures_context = os.path.join(os.environ['WORKSPACE'], 'build_output', 
+                                                'buildfailures-with-context.txt')
         test_failures = os.path.join(os.environ['WORKSPACE'], 'test_output', 'testfailures.txt')
         body = ''
         if os.path.exists(build_failures):
